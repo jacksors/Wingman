8a3a8bb9f06bdebc77633cd36a6d4ad963fec364
Jackson Stone <jackson@jstone.us>
2024-01-27 17:48:11 -0600

    import prisma

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import type {User} from '@prisma/client'
+import prisma from '@/lib/prisma'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
