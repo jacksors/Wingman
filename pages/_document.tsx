02e6991ecf98883c7dc24f75a9ecac890a84c747
Aiden Stickney <stickneyaiden@gmail.com>
2024-01-27 16:59:31 -0600

    Home Page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { cn } from "@/lib/utils";
 
-import { cn } from "@/lib/utils"
 
 export default function Document() {
   return (
     <Html lang="en" suppressHydrationWarning>
       <Head />
       <body>
-        <Main />
-        <NextScript />
+          <Main />
+          <NextScript />
       </body>
     </Html>
   );
