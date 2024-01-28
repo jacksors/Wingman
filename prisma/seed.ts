505e7b02048b70a8c5559a831399b55fb2b6c007
Jackson Stone <jackson@jstone.us>
2024-01-28 10:21:21 -0600

    update seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -18,7 +18,10 @@ async function createAirports() {
     const airports = [
         { code: 'JFK', name: 'John F. Kennedy International Airport', location: 'New York' },
         { code: 'LAX', name: 'Los Angeles International Airport', location: 'Los Angeles' },
-        { code: 'ORD', name: "O'Hare International Airport", location: 'Chicago' }
+        { code: 'ORD', name: "O'Hare International Airport", location: 'Chicago' },
+        { code: 'DFW', name: 'Dallas/Fort Worth International Airport', location: 'Dallas' },
+        { code: 'DEN', name: 'Denver International Airport', location: 'Denver' },
+        { code: 'SFO', name: 'San Francisco International Airport', location: 'San Francisco' },
     ];
     for (const airport of airports) {
         await prisma.airport.create({ data: airport });
@@ -29,7 +32,11 @@ async function createUsers() {
     const users = [
         { id: 'seed-user-1', username: 'user1', firstName: 'John', lastName: 'Doe', email: 'johndoe@example.com' },
         { id: 'seed-user-2', username: 'user2', firstName: 'Jane', lastName: 'Doe', email: 'janedoe@example.com' },
-        { id: 'seed-user-3', username: 'user3', firstName: 'Jim', lastName: 'Beam', email: 'jimbeam@example.com' }
+        { id: 'seed-user-3', username: 'user3', firstName: 'Jim', lastName: 'Beam', email: 'jimbeam@example.com' },
+        { id: 'seed-user-4', username: 'user4', firstName: 'Jack', lastName: 'Daniels', email: 'test@test.com' },
+        { id: 'seed-user-5', username: 'user5', firstName: 'John', lastName: 'Smith', email: 'test2@test.com' },
+        { id: 'seed-user-6', username: 'user6', firstName: 'Jane', lastName: 'Smith', email: 'test3@test.com'},
+        { id: 'seed-user-7', username: 'user7', firstName: 'John', lastName: 'Appleseed', email: 'test4@test.com'}
     ];
     for (const user of users) {
         await prisma.user.create({ data: user });
@@ -65,16 +72,33 @@ async function createRoutes() {
 
 async function createFlights() {
     const routes = await prisma.route.findMany();
-    console.log(routes);
     const tailNumbers = await prisma.tailNumber.findMany();
     const airlines = await prisma.airline.findMany();
     const users = await prisma.user.findMany();
 
     for (const route of routes) {
         for (const airline of airlines) {
-            const departTime = new Date();
-            const arriveTime = addHours(departTime, Math.floor(Math.random() * 6) + 3);
+            let departTime = new Date();
+            let arriveTime = addHours(departTime, Math.floor(Math.random() * 6) + 3);
 
+            let flightPrefix = airline.name === 'American Airlines' ? 'AA' : airline.name === 'Delta Air Lines' ? 'DL' : 'UA';
+
+            await prisma.flight.create({
+                data: {
+                    flightNumber: `${flightPrefix}${Math.floor(Math.random() * 1000)}`,
+                    airlineId: airline.id,
+                    tailNumberId: tailNumbers[Math.floor(Math.random() * tailNumbers.length)].id,
+                    routeId: route.id,
+                    departTime: departTime,
+                    arriveTime: arriveTime,
+                    users: {
+                        connect: users.map(user => ({ id: user.id }))
+                    }
+                }
+            });
+
+            departTime = new Date();
+            arriveTime = addHours(departTime, Math.floor(Math.random() * 6) + 3);
             await prisma.flight.create({
                 data: {
                     flightNumber: `AA${Math.floor(Math.random() * 1000)}`,
